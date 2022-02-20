42a73d5e9453d8bf6530c32bb666eb1a814519fc
Oliver01314 <1003376947@qq.com>
2022-02-20 15:09:38 +0000

    chapter7

--- rango/forms.py
+++ rango/forms.py
@@ -1,17 +1,17 @@
-from socket import fromshare
-from unicodedata import name
+
 from django import forms 
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-    # 不懂为什么这样不写在一行，不知道会不会报错
+    
     name = forms.CharField(max_length=128, 
                            help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
-# create inline class
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
     class Meta:
-        # provide association between the modelform and a model
+       
         model = Category
         fields = ('name',)
 
